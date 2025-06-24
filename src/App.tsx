46d1cc84afc40116b474796f60c5de141f650bd2
Edgar Francis Caballero <efrcaballero@gmail.com>
2025-06-23 21:33:20 -0600

    small change to button border colour

--- src/App.tsx
+++ src/App.tsx
@@ -66,7 +66,7 @@ const App = () => {
               ))}
           </ul>
           <button
-          className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500  text-zinc-100 px-2 py-1 cursor-pointer" onClick={onSubmit}>
+          className="rounded-r-md border-2 border-black hover:border-zinc-500 hover:text-zinc-500  text-zinc-100 px-2 py-1 cursor-pointer" onClick={onSubmit}>
           search
           </button>
         </div>
