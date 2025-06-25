7f7b518c6e109a48374d75a035f44bbf6abf99ef
Edgar Francis Caballero <efrcaballero@gmail.com>
2025-06-24 23:55:31 -0600

    Adding search component

---
+++ src/components/Search.tsx
@@ -0,0 +1,48 @@
+type Props = {
+  term: string
+}
+const Search = ({
+  term, 
+  options, 
+  onInputChange, 
+  onOptionSelect, 
+  onSubmit
+}) => {
+ 
+  return (
+
+    <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
+      
+      <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-14 h-full lg:h-[500px] bg-white/20 backdrop-blur-lg rounded drop-shadow-lg text-zinc-700">
+        <h1 className="text-4xl font-thin">Weather
+          <span className="font-black"> Forecast</span> 
+        </h1>
+        <p className="text-sm mt-2">Enter Below a place you want to the weather of and select an option from the dropdown</p>
+        <div className="flex mt-10 md:mt-4 relative">
+          <input type="text" value={term} className="px-2 py-1 rounded-l-md border-2 border-white bg-white" 
+          onChange={onInputChange}
+          />
+          <ul className="absolute top-9 bg-white ml-1 rounded-b-md">
+            {options.map((option: optionType, index:number) => (
+              <li key={option.name + '-' + index}>
+                <button className="text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-1 cursor-pointer" 
+                onClick={() => onOptionSelect(option)}>
+                  {option.name}
+                </button>
+              </li>
+              
+              ))}
+          </ul>
+          <button
+          className="rounded-r-md border-2 border-black hover:border-zinc-500 hover:text-zinc-500  text-zinc-100 px-2 py-1 cursor-pointer" onClick={onSubmit}>
+          search
+          </button>
+        </div>
+        
+      </section>
+    </main>
+
+  )
+}
+
+export default Search
