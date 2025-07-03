696291d749bb6b4e36ebd74fb4e568cf850606d2
Edgar Francis Caballero <efrcaballero@gmail.com>
2025-07-03 03:28:34 -0600

    added more tiles to show humidity, pressure, visibility, precipitation etc.

--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -1,4 +1,4 @@
-import { getSunTime, getWindDirection } from "../helpers"
+import { getHumidityValue, getPop, getSunTime, getVisibilityValue, getWindDirection } from "../helpers"
 import { forecastType } from "../types"
 import Sunrise from "./Icons/Sunrise"
 import Sunset from "./Icons/Sunset"
@@ -54,17 +54,57 @@ const Forecast = ({data}: Props) => {
           <div className="w-[140px] text-xs font-bold flex flex-col items-center bg-white/20 backdrop-blur-ls rounded drop-shadow-lg py-4 mb-5" >
             <Sunset /> <span className="mt-2">{getSunTime(data.sunset)}</span>
           </div>
+
           {/* {Wind} */}
           <Tile 
           icon="wind" 
           title="Wind" 
           info={`${Math.round(today.wind.speed)} km/hr`}
           description={`${getWindDirection(Math.round(today.wind.deg))}, gusts ${today.wind.gust.toFixed(1)} km/hr`} />
+
           {/* {Feels Like} */}
+          <Tile 
+          icon="feels" 
+          title="Feels like" 
+          info={<Degree temp={Math.round(today.main.feels_like)}/>}
+          description={`Feels ${
+            Math.round(today.main.feels_like) < Math.round(today.main.temp) ? 'colder' : 'warmer'
+          }`}
+          />
+
           {/* {Humidity} */}
+          <Tile 
+          icon="humidity" 
+          title="Humidity" 
+          info={`${today.main.humidity} %`}
+          description={getHumidityValue(today.main.humidity)}
+          />
+
           {/* {Pop} */}
+          <Tile 
+          icon="pop" 
+          title="Precipitation" 
+          info={`${Math.round(today.pop)} %`}
+          description={`${getPop(today.pop)}, clouds at ${today.clouds.all}%`}
+          />
+
           {/* {Pressure} */}
+          <Tile 
+          icon="pressure" 
+          title="Pressure" 
+          info={`${today.main.pressure} hPa`}
+          description={`${
+            Math.round(today.main.pressure) < 1013 ? 'Lower' : 'Higher'
+          } than standard`}
+          />
+
           {/* {Visibility} */}
+          <Tile 
+          icon="visibility" 
+          title="Visibility" 
+          info={`${(today.visibility / 1000).toFixed()} km`}
+          description={getVisibilityValue(today.visibility)}
+          />
         </section>
       </div>
     </div>
